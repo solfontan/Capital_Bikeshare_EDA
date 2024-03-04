ad6881816ac0efdf9eb775f9d0fb50bb24922d48
Sol Fontán González <139863383+solfontan@users.noreply.github.com>
2024-03-04 16:07:57 -0300

    Update proyecto EDA

--- src/utils/mis_funciones.py
+++ src/utils/mis_funciones.py
@@ -4,6 +4,52 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.stats import kurtosis, skew
 
+def cardinalidad(df: pd.DataFrame):
+    cardi = pd.DataFrame(columns=['cardinalidad', 'porcentaje_cardinalidad', 'tipo_de_dato', 'valores_unicos', 'tipo_de_variable'],
+                         index=df.columns)
+
+    cardi['cardinalidad'] = [df[col].nunique() for col in df.columns]
+    cardi['porcentaje_cardinalidad'] = cardi['cardinalidad'] / len(df) * 100
+    cardi['tipo_de_dato'] = df.dtypes
+    cardi['valores_unicos'] = [valor if valor <= 15 else 'valores unicos no representativos' for valor in
+                               [df[columna].nunique() for columna in df.columns]]
+
+    valores_tipo_variable = [input(f'Para la columna {columna} ingrese el valor "Tipo_de_variable": ') for columna in df.columns]
+    cardi['tipo_de_variable'] = valores_tipo_variable
+
+    return cardi
+
+def extended_describe(column, df):
+    describe_df = df[column].describe()
+    # Crear un nuevo DataFrame con las estadísticas extendidas
+    extended_describe_df = pd.DataFrame({
+        'count': describe_df['count'],
+        'mean': describe_df['mean'],
+        'median':df[column].median(),
+        'mode' : df[column].mode()[0],
+        'std' : round(describe_df['std'],2),
+        'min': describe_df['min'],
+        '25%': describe_df['25%'],
+        '50%': describe_df['50%'],
+        '75%': describe_df['75%'],
+        'max': describe_df['max'],
+        'kurtosis': round(kurtosis(df[column]), 2),
+        'skewness': round(skew(df[column]),2)
+    }, index=[column])
+    
+    if kurtosis(df[column]) > 0 :
+        print(f"La distribución es leptocúrtica con una curtosis de {round(kurtosis(df[column]), 2)}. Los datos se encuentran concentrados alrededor de la media.")
+    elif kurtosis(df[column]) < 0 :
+        print(f"La distribución es platicúrtica con una curtosis de {round(kurtosis(df[column]), 2)}. Los datos se encuentran dispersos.")
+    elif kurtosis(df[column]) == 0 :
+        print(f"La distribución es mesocúrtica con una curtosis de {round(kurtosis(df[column]), 2)}. Los datos se comportan de manera normal")
+        
+    if skew(df[column]) > 0 :
+        print(f"La distribución se encuentra sesgada hacia la izquierda {round(skew(df[column]),2)}.")
+    else:
+        print(f"La distribución se encuentra sesgada hacia la derecha {round(skew(df[column]),2)}.")
+        
+    return extended_describe_df
 
 def grafico_univariante(df : pd.DataFrame, columna : str, color_boxplot='#b81414', color_hist='#b81414'):
     """ Pasamos un DataFrame con su columna, para que nos retorne dos gráficos.
@@ -47,27 +93,13 @@ def grafico_univariante(df : pd.DataFrame, columna : str, color_boxplot='#b81414
     pos.y0 = 1.02
     ax.set_position(pos)
 
-    ax_hist.set_ylabel('Frequency');
+    ax_hist.set_ylabel('Frecuencia')
+    extended_description_df = extended_describe(columna, df)  # Llamada a la función extended_describe
     
-    print(f"kurtosis: {kurtosis_valor:.2f}")
-    print(f"simetria: {simetria_valor:.2f}")
-
-    if kurtosis_valor > 3:
-        print("La distribución es leptocúrtica, lo que sugiere colas pesadas y picos agudos.")
-    elif kurtosis_valor < 3:
-        print("La distribución es platicúrtica, lo que sugiere colas ligeras y un pico achatado.")
-    else:
-        print("La distribución es mesocúrtica, similar a una distribución normal.")
-
-    if simetria_valor > 0:
-        print("La distribución es asimétrica positiva (sesgo hacia la derecha).")
-    elif simetria_valor < 0:
-        print("La distribución es asimétrica negativa (sesgo hacia la izquierda).")
-    else:
-        print("La distribución es perfectamente simétrica alrededor de su media.")
+    return extended_description_df
     
     
-def grafico_bivariante(df : pd.DataFrame, x : str, y: str, forma : str, color_jointplot='#b81414', title=None) -> sns.jointplot:
+def grafico_bivariante(df : pd.DataFrame, x : str, xname: str,  y: str, yname :str,  forma : str, color_jointplot='#b81414', title=None) -> sns.jointplot:
     """ Grafico bivariante con el fin de retornar un jointplot entre variables para un análisis más práctico
     arg:
     df : DataFrame
@@ -86,6 +118,9 @@ def grafico_bivariante(df : pd.DataFrame, x : str, y: str, forma : str, color_jo
         plot = sns.jointplot(data = df, x = x, y = y, kind=forma, marginal_ticks=True, color=color_jointplot)
         plot.fig.suptitle(title, y=1.05)
 
+        plt.xlabel(xname)
+        plt.ylabel(yname)
+        
         return plot
     else:
         print("Error en el tipo de gráfica")
